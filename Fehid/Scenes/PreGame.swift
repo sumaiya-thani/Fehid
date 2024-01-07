ba4a37d1dab27b80302900d285b800fc52a09b75
sumaiya-thani <sumaiyathani@gmail.com>
2024-01-07 19:59:14 +0300

    Update the Story , Pre Story , Score

---
+++ Fehid/Scenes/PreGame.swift
@@ -0,0 +1,8 @@
+//
+//  PreGame.swift
+//  Fehid
+//
+//  Created by sumaiya on 05/01/2567 BE.
+//
+
+import Foundation
---
+++ Fehid/Scenes/PrograssScene.swift
@@ -0,0 +1,8 @@
+//
+//  PrograssScene.swift
+//  Fehid
+//
+//  Created by sumaiya on 28/12/2566 BE.
+//
+
+import Foundation
